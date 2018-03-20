d0ed2313c6c9d920d0ffe194204933a45b04f9dd
Rise <ritm-temp@hotmail.com>
2018-03-20 10:17:15 +0600

    Lab5 v1.0

---
+++ Lab5/Lab5/Inserter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab5
+{
+    class Inserter
+    {
+        public List<int> Insert(List<int> array)
+        {
+            int difference = DifferenceBetweenMaxAndMin(array);
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (array[i] < 0 && array[i] % 2 == 0)
+                {
+                    i++;
+                    array.Insert(i, difference);
+                }
+            }
+            return array;
+        }
+
+        public int DifferenceBetweenMaxAndMin(List<int> array)
+        {
+            int difference = array.Min() - array.Max();
+            return difference;
+        }
+    }
+}
---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new List<int> { -12, -3, -4, 5, 3, 4, 5, -12, -4, -5, 6, 5, 4 };
+            foreach (int element in array)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.WriteLine();
+            Inserter inserter = new Inserter();
+            List<int> resultArray = inserter.Insert(array);
+            foreach(int element in resultArray)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.Read();
+        }
+    }
+}
